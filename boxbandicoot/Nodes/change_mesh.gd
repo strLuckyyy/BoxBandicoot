82098ff959e98c7c6f78d8546184140029dfa3b1
Abrahão Francis <goncalvesabrahao@edu.unisinos.br>
2024-11-16 16:15:02 -0300

    Change Mesh System done

---
+++ boxbandicoot/Nodes/change_mesh.gd
@@ -0,0 +1,75 @@
+extends Node3D
+
+## Não precisa chamar nenhum método dessa classe para ela funcionar. Apenas coloque ela no Nó do 
+## player e conecte as variáveis necessárias.
+class_name Change_Node
+
+@export var _player_mesh: MeshInstance3D
+@export var _box_material: Material
+@export var _prism_material: Material
+@export var _sphere_material: Material
+@export var _skills_node: Skills_Node
+
+@onready var _is_box: bool = false
+@onready var _is_sphere: bool = false
+@onready var _is_prism: bool = false
+
+
+func _physics_process(_delta: float) -> void:
+	_change_mesh()
+
+
+## Método responsável por transformar o player em alguma forma com base no Input do mesmo.
+func _change_mesh() -> void:
+	_update_current_mesh()
+	
+	if Input.is_action_just_pressed("TransformToBox"):
+		if _is_box == true: return
+		_skills_node.y_direction = 1
+		_skills_node.is_prism = false
+		_transform_mesh(BoxMesh)
+	
+	if Input.is_action_just_pressed("TransformToSphere"):
+		if _is_sphere == true: return
+		_skills_node.y_direction = 1
+		_skills_node.is_prism = false
+		_transform_mesh(SphereMesh)
+	
+	if Input.is_action_just_pressed("TransformToPrism"):
+		if _is_prism == true: return
+		_transform_mesh(PrismMesh)
+
+
+## Encapsulando para evitar repetição. Aqui muda a mesh e o material do objeto.
+func _transform_mesh(mesh: Object) -> void:
+	_player_mesh.set_mesh(mesh.new())
+	_update_current_mesh()
+	_player_mesh.mesh.set_material(_setting_material())
+
+
+## Evitar do objeto se transformar nele mesmo.
+func _update_current_mesh() -> void:
+	# Checando Box State
+	_setting_mesh("BoxMesh", true, false, false)
+	
+	# Checando Prism State
+	_setting_mesh("PrismMesh", false, true, false)
+	
+	# Checando Sphere State
+	_setting_mesh("SphereMesh", false, false, true)
+
+
+## Encapsulando para evitar repetição. Esse método define as variáveis booleanas para o funcionamento
+## do sistema.
+func _setting_mesh(mesh_name: String, box_value: bool, prism_value: bool, sphere_value: bool) -> void:
+	if _player_mesh.mesh.to_string().contains(mesh_name):
+		_is_box = box_value
+		_is_prism = prism_value
+		_is_sphere = sphere_value
+
+
+## Retorna o material correspondente à malha atual.
+func _setting_material() -> Material:
+	if _is_box: return _box_material
+	if _is_prism: return _prism_material
+	return _sphere_material
--- boxbandicoot/Nodes/skills_node.gd
+++ boxbandicoot/Nodes/skills_node.gd
@@ -6,23 +6,26 @@ class_name Skills_Node
 @export var _stamina_node: Stamina_Node
 @export var _sphere_stamina_cost_cooldown: Timer
 
-@onready var _is_prism: bool = false
+@onready var is_prism: bool = false
 @onready var _is_sphere: bool = false
 @onready var _can_waste: bool = true
 @onready var _jump_count: int = 0
-@onready var _y_direction: int = 1
+@onready var y_direction: int = 1
 
 const _JUMP_VELOCITY: float = 9.6
 const _FALL_VELOCITY: float = 2.5
 
 func _physics_process(delta: float) -> void:
+	
 	# Execução da habilidade em esfera. A habilidade funciona enquanto o jogador segura o botão.
 	if _is_sphere and _stamina_node.can_waste == true: _player.velocity.y += 0.35
 	elif not _player.is_on_floor(): _gravity(delta) 
 	
 	# Execução da habilidade em prisma. Literalmente, inverte a gravidade.
-	if _is_prism: _gravity(delta)
+	if is_prism: _gravity(delta)
 	elif not _player.is_on_floor(): _gravity(delta)
+	
+	print(y_direction, is_prism)
 
 
 ## Método que deve ser chamado quando for usar alguma habilidade. Esse método "lê"
@@ -44,7 +47,7 @@ func realese_button_skill() -> void: _is_sphere = false
 
 
 func _gravity(delta: float) -> void: 
-	_player.velocity += _player.get_gravity() * delta * (_y_direction)
+	_player.velocity += _player.get_gravity() * delta * (y_direction)
 
 
 ## Não chamar esse método! Use "use_skill" ao invés disso.
@@ -52,32 +55,32 @@ func _box_shape_skill() -> void:
 	# Pulo.
 	if _player.is_on_floor():
 		_player.velocity.y = _JUMP_VELOCITY
-		_stamina_node.waste(15)
+		#_stamina_node.waste(15)
 		_jump_count += 1
 	
 	# Se tacando no chão caso esteja no ar.
 	elif _jump_count == 1 and !_player.is_on_floor():
 		_player.velocity.y = _JUMP_VELOCITY * (-_FALL_VELOCITY)
-		_stamina_node.waste(5)
+		#_stamina_node.waste(5)
 		_jump_count = 0
 
 
 ## Não chamar esse método! Use "use_skill" ao invés disso.
-func _sphere_shape_skill() -> void: 
+func _sphere_shape_skill() -> void:
 	_is_sphere = true
 	
 	if _can_waste == false: return 
 	
-	_stamina_node.waste(25)
+	#_stamina_node.waste(25)
 	_sphere_stamina_cost_cooldown.start(_sphere_stamina_cost_cooldown.get_wait_time())
 	_can_waste = false
 
 
 ## Não chamar esse método! Use "use_skill" ao invés disso.
 func _prism_shape_skill() -> void:
-	_is_prism = true if _is_prism == false else false
-	_y_direction = 1 if _y_direction == -1 else -1
-	_stamina_node.waste(30)
+	is_prism = true if is_prism == false else false
+	y_direction = 1 if y_direction == -1 else -1
+	#_stamina_node.waste(30)
 
 
 func _on_sphere_stamina_timeout() -> void:
