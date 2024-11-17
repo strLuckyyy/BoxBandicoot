a950e29aa6a0f77a67fec3f3ac44b8aac33b79a8
Abrahão Francis <goncalvesabrahao@edu.unisinos.br>
2024-11-17 13:35:05 -0300

    add setters in stamina class

--- boxbandicoot/Nodes/stamina_node.gd
+++ boxbandicoot/Nodes/stamina_node.gd
@@ -42,8 +42,19 @@ func recover_with_value(value: int) -> void:
 	_current_energy += value
 
 
+## Aqui você setta um novo tempo X de recuperação. Você pode fazer o personagem recuperar mais 
+## lentamente ou rapidamente. Tempo em segundos.
+func set_recover_cooldown(new_cd: float) -> void:
+	_recover_timer.set_wait_time(new_cd)
+
+
+## Aqui você setta um novo tempo X em que o personagem vai se manter no TiredState. Tempo em segundos.
+func set_tired_cooldown(new_cd: float) -> void:
+	_tired_timer.set_wait_time(new_cd)
+
+
 ## Não chame esse método, ele já é chamado a todo momento.
-## Recupera a vida a cada X tempo. Esse valor X deve ser mudado pelo Node RecoverTimer em si.
+## Recupera a vida a cada X tempo. Esse valor X deve ser mudado pelo Node RecoverTimer em si ou pelo set.
 func _recover() -> void:
 	if _is_timer_over == false: return
 	if can_waste == false: return
@@ -57,7 +68,7 @@ func _recover() -> void:
 
 ## Não chame esse método, ele já é chamado a todo momento.
 ## Inicia o timer para sair do estado "tired". Esse estado impede a recarga de 
-## energia por um periodo de tempo X. Esse valor X deve ser mudado pelo Node TiredTimer em si.
+## energia por um periodo de tempo X. Esse valor X deve ser mudado pelo Node TiredTimer em si ou pelo set.
 func _tired_state() -> void: 
 	if _is_timer_over == false: return
 	if _is_tired == false: return
