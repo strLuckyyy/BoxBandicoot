aeb40618f524d1a018990b76040193b5bda10a7b
Diaskiert <116126379+Diaskiert@users.noreply.github.com>
2024-11-18 01:34:14 -0300

    Menu de pausa e UI
    Lembrem-se apenas de retirar do nodo do jogador, a linha que prende o mouse ao clicar na tela, ela foi melhor implementada no menu de pause

---
+++ boxbandicoot/Scripts/gameplay_ui.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@export var stamina_node: Stamina_Node
+@export var mesh: MeshInstance3D
+
+@onready var text_edit: TextEdit = $ColorRect/TextEdit
+@onready var mesh_text: TextEdit = $ColorRect2/TextEdit
+
+var last_stamina = 0
+var last_mesh = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	UI_Update_Form
+	last_stamina = stamina_node._current_energy
+	text_edit.text = "Stamina: " + str(stamina_node._current_energy)
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if last_stamina != stamina_node._current_energy:
+		update_stamina_UI()
+	UI_Update_Form()
+	
+func update_stamina_UI():
+	last_stamina = stamina_node._current_energy
+	text_edit.text = "Stamina: " + str(stamina_node._current_energy)
+
+func UI_Update_Form():
+	if mesh.mesh.to_string().contains("BoxMesh"):
+			mesh_text.text = "Type: Box" 
+		
+	elif mesh.mesh.to_string().contains("SphereMesh"):
+		mesh_text.text = "Type: Sphere" 
+	
+	elif mesh.mesh.to_string().contains("PrismMesh"):
+		mesh_text.text = "Type: Prism" 
---
+++ boxbandicoot/Scripts/pause_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+@export var player: Player_node
+@onready var button: Button = $Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.visible = false
+	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player.is_paused == true:
+		self.visible = true
+		self.mouse_filter = Control.MOUSE_FILTER_STOP
+	else:
+		self.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		self.visible = false
+	
+func _on_button_pressed():
+	player.is_paused = false
